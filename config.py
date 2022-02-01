a72fafb391bd6f86f3e85ebdedb0c0bb2aa882e1
joshxinjie <leexinjie@gmail.com>
2022-02-01 15:21:59 +0800

    change config to heroku database

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # local or heroku
-DEPLOYMENT="local"
+DEPLOYMENT="heroku"
 
 LOCAL_SQLALCHEMY_DATABASE_URI="postgresql://myuser:password@localhost:5432/casting"
 
-HEROKU_SQLALCHEMY_DATABASE_URI=""
+HEROKU_SQLALCHEMY_DATABASE_URI="postgres://focudljubvikoc:58c505f1eb03c923464800a25a01211d7a352c026eaa6fbea27348383976bb9b@ec2-50-19-32-96.compute-1.amazonaws.com:5432/d2b7s41ijh0mbb"
